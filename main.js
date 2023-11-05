3479483660944f7d41845a0d6dee4cc621bdd780
newhandarky <newhandarky@gmail.com>
2023-11-05 22:49:03 +0800

    修改路徑

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-import "./js/index";
-import "./js/user";
-import "./js/admin";
+// import "./js/index";
+// import "./js/user";
+// import "./js/admin";
