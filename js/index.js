734b31d5f5da7985902306f4cf5cc529f86c08c8
newhandarky <newhandarky@gmail.com>
2023-11-05 14:34:03 +0800

    修改JS路徑

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-
+import "./admin";
+import "./user";
 
 let adminBtn = document.querySelector(".adminBtn");
 let userBtn = document.querySelector(".userBtn");
@@ -33,31 +34,3 @@ userBtn.addEventListener("click", function(){
         console.log(err.response);
     })
 })
-
-
-//User
-let welcome = document.querySelector(".welcome");
-
-welcome.innerHTML = `歡迎回來 ${localStorage.getItem("userName")}`;
-
-document.querySelector('.btn').addEventListener('click', function () {
-    var formData = new FormData();
-    formData.append("image", document.querySelector(".myFile").files[0]);
-
-    axios.post("https://api.imgur.com/3/image", formData, {
-        headers: {
-            "Authorization": "Client-ID 9370a32fefaf63c",
-            "Content-Type": "multipart/form-data"
-        }
-    })
-        .then(function (response) {
-            var photo = response.data.data.link;
-            // var photo_hash = response.data.data.deletehash;
-            document.querySelector(".pic").src = photo;
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.error("發生錯誤", error);
-        });
-});
-
--- main.js
+++ main.js
@@ -2,8 +2,6 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import "./js/index";
-
-
 // import "./js/user";
 // import "./js/admin";
 
