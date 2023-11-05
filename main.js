4fdba21cb5e6750b0dbb9b858f92a874c5f4b636
newhandarky <newhandarky@gmail.com>
2023-11-05 13:21:27 +0800

    調整JS檔案路徑

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-import "./js/admin.js"
-import "./js/index.js"
-import "./js/user.js"
+import "./js/admin.js";
+import "./js/index.js";
+import "./js/user.js";
